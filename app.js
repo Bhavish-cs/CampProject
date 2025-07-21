184bcd6fb6bdfe9d8ffa5ba5ad0ffd7762c188ea
Bhavish-cs <bhavish.gowda2k4@gmail.com>
2025-07-22 04:27:26 +0530

    changes

--- app.js
+++ app.js
@@ -32,9 +32,19 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
+
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
 // Route to create a new campground
+
+app.get('/campgrounds/:id', async (req, res,) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+});
 
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
-});
+}); 
