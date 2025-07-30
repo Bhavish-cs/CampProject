9c87d1ebd5f9acb312a93cf939cbe0417bea0bb6
Bhavish-cs <bhavish.gowda2k4@gmail.com>
2025-07-30 23:54:54 +0530

    Fix campground image fetching

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import axios from 'axios';
 import Campground from '../models/campground.js';
 import { places, descriptors } from './seedHelpers.js';
 import cities from './cities.js';
 
-dotenv.config();
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
     .then(() => console.log("Database Connected"))
@@ -13,37 +10,24 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-const getImageFromUnsplash = async () => {
-    try {
-        const res = await axios.get('https://api.unsplash.com/photos/random', {
-            params: {
-                query: 'camping', // You can change this to "mountain", "forest", etc.
-                orientation: 'landscape'
-            },
-            headers: {
-                Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`
-            }
-        });
-        return res.data.urls.regular;
-    } catch (err) {
-        console.error("Error fetching from Unsplash:", err.message);
-        // fallback image
-        return 'https://source.unsplash.com/collection/483251/1600x900';
-    }
+// Generate random campground images using Picsum
+const getRandomCampgroundImage = () => {
+    const imageId = Math.floor(Math.random() * 1000) + 1;
+    return `https://picsum.photos/800/600?random=${imageId}`;
 };
 
 const seedDB = async () => {
     await Campground.deleteMany({});
 
-    for (let i = 0; i < 20; i++) {  // Fetching fewer to avoid API rate limits
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * cities.length);
-        const imgUrl = await getImageFromUnsplash(); // ⛏️ await image fetch here
+        const imgUrl = getRandomCampgroundImage();
 
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
             price: Math.floor(Math.random() * 50) + 10,
             image: imgUrl,
-            description: 'Seeded campground from Unsplash API',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             location: `${cities[random1000].city}, ${cities[random1000].state}`
         });
 
