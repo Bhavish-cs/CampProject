a68fce2428daa5438a2ed40c1092fb69ab68812a
Bhavish-cs <bhavish.gowda2k4@gmail.com>
2025-09-11 23:49:09 +0530

    lottifiles loader implemented

--- app.js
+++ app.js
@@ -474,7 +474,7 @@ app.use((err, req, res, next) => {
 
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Serving on port ${PORT}`);
 });
